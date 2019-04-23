f7bba8e2be12bbbff32cf66677108b0196f5bf35
sylvandor <scottwwj@gmail.com>
2019-04-23 17:38:46 -0500

    Set random image for ball on load

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber: Int = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        randomBallNumber = Int.random(in: 0 ... 4)
+        
+        updateBall()
     }
 
-
+    @IBAction func askButtonPressed() {
+        
+    }
+    
+    func updateBall() {
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
 }
 
